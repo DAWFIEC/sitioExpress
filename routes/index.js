3943c6fb8c69a9bf41c500ff10051987443f7989
aavendan <aavendan@gmail.com>
2021-08-02 21:55:05 -0500

    new view: productos

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'My Dashboard' });
 });
 
+router.get('/productos', function(req, res, next) {
+  res.render('productos', { title: 'My Dashboard :: Productos' });
+});
+
 module.exports = router;
