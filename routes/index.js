cce8b0aa55f55fc98d87c96193b6a7a33dadd76f
aavendan <aavendan@gmail.com>
2021-08-03 19:58:18 -0500

    basic

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+const Sequelize     = require('sequelize');
+const Producto       = require('../models').producto;
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'My Dashboard' });
 });
 
 router.get('/productos', function(req, res, next) {
-  res.render('productos', { title: 'My Dashboard :: Productos' });
+
+  Producto.findAll({
+      attributes: { exclude: [ "updatedAt"] }
+    })
+    .then(productos => {
+      res.render('productos', { title: 'My Dashboard :: Productos', productos: productos });
+    })
+    .catch(error => res.status(400).send(error))
+
+  
 });
 
 module.exports = router;
