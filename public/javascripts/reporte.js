bc07d25f28501be23ab8e4d27050b0d5b39a31fe
aavendan <aavendan@gmail.com>
2021-08-17 19:20:38 -0500

    ready

--- public/javascripts/reporte.js
+++ public/javascripts/reporte.js
@@ -1,4 +1,19 @@
 window.onload = () => {
-  //Mediante un fetch al URL "http://localhost:3001/clientes" cargar select#inputGroupSelect01 
-  //Mediante un fetch al URL "http://localhost:3001/ordenes" cargar table#ordenes
+  /*
+    Realice un requerimiento asincrónico al URL "http://localhost:3001/clientes".
+    Cargue el resultado en el elemento select#inputGroupSelect01
+  */
+
+  
+
+
+ /*
+  Reaccione al evento change del elemento select#inputGroupSelect01. (REFERENCIA: https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/change_event)
+  Realice un requerimiento asincrónico al URL "http://localhost:3002/ordenes/:idCliente" con el identificador del cliente seleccionado.
+  Cargue los resultados en el elemento table#ordenes_tablas.
+ */
+
+ 
+
+
 };
